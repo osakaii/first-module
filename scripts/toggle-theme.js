351304d9b2b44fa6585535cb536d9f6b25d84ac8
Muhammad <mdzhanov@gmail.com>
2022-05-19 13:05:56 +0600

    remastered toggle-theme function

--- scripts/toggle-theme.js
+++ scripts/toggle-theme.js
@@ -3,33 +3,39 @@ const orange = "#FF9209";
 const white = "#fff";
 const dark = "#222";
 
-
-
-export const toggleTheme = (mode) => {
+export const toggleTheme = ( mode ) => {
+    
   const darkMode = document.querySelector(".darkmode");
   const text = darkMode.querySelectorAll("p");
-  const svg = darkMode.querySelectorAll("svg");
+
+  const lightSvg = darkMode.querySelectorAll("svg")[0];
+  const darkSvg = darkMode.querySelectorAll("svg")[1];
+
+  const changeTextColor = (color, btnIndex) =>{
+    text.forEach((item, index) => {
+        item.style.color = color;
+        if (index === btnIndex) {
+          item.style.color = orange;
+        }
+      });
+  }
 
   if (mode === "light") {
     darkMode.style.background = white;
-    text.forEach((item, index) => {
-      item.style.color = dark;
-      if (index === 3) {
-        item.style.color = orange;
-        svg[0].style.fill = orange;
-        svg[1].style.fill = dark;
-      }
-    });
+    lightSvg.style.fill = orange;
+    darkSvg.style.fill = dark;
+
+    const lightBtnIndex = 3
+
+    changeTextColor(dark, lightBtnIndex)
   }
   if (mode === "dark") {
     darkMode.style.background = dark;
-    text.forEach((item, index) => {
-      item.style.color = white;
-      if (index === 4) {
-        item.style.color = orange;
-        svg[1].style.fill = orange;
-        svg[0].style.fill = white;
-      }
-    });
+    darkSvg.style.fill = orange;
+    lightSvg.style.fill = white;
+
+    const darkBtnIndex = 4
+
+    changeTextColor(white, darkBtnIndex)
   }
 };
