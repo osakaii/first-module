8c47ade66b508ae33b5a2b4fb2e40207fe3abe8c
Muhammad <mdzhanov@gmail.com>
2022-06-02 15:39:43 +0600

    pixel perfect

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: {
         main: './src/scripts/main.js'
     },
